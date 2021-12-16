72cfbea0c86d526685124a1c398e396f2174a666
Akram Nadri <akram.nadri@gmail.com>
2021-12-16 17:36:00 -0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,54 @@
+import javax.swing.event.SwingPropertyChangeSupport;
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class Test{
+
+
+    public static void main(String[]args){
+        System.out.println("hello Ak");
+
+        String ak = "Akram";
+        String aN = "An";
+        System.out.println("first name is " + ak + " and friend is " + aN);
+    }
+
+
+    
+} 
+
+// PRIMITIVE            VS     REFERENCE
+// 8 types                      unlimited
+// stores data                  store and address
+// can only hold 1 value        could hold more then 1 value
+// less memory                  more memory
+// fast                         slower
+
+
+// PRIMITIVES
+// Boolean - 1 bit - true or false
+// byte - 1 byte - -128 to 127
+// short - 2 bytes - -32,768 to 2 billion
+// long - 8 bytes - -9 quintil to 9 quint
+// float 4 bytes - fractional number - 6.7 number after decimal 
+// double - 8 bytes - fractional number up to 15 digits after decimal ex: 3.1245133145975621
+// char - 2 bytes - single character/letter/ASCII value
+
+// REFERENCE
+// string - varies - a sequence of characters ex: "hello hi"
+
+
+// How to declare a variable
+
+// Decleration
+// int x;
+
+// Assignment 
+// x = 123;
+
+// Initialization
+// int x = 123;
+
+// testing 1234
+
+// 1234XXXXXX
+
